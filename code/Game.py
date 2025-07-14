41c43545cd56e87d8b696707a68a47585130c613
marcelinho <marcelo.lhp@gmail.com>
2025-07-14 12:41:47 -0300

    criação do menu de interações Animação do background

--- code/Game.py
+++ code/Game.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Level import Level
 from code.Menu import Menu
 class Game:
     def __init__(self):
@@ -8,50 +9,20 @@ class Game:
         self.window = pygame.display.set_mode(size=(WIN_WIDTH,WIN_HEIGHT ))
 
 
-    def run(self, ):
+    def run(self):
         # Loop principal do jogo
-        rodando = True
-        while rodando:
+       while True:
             menu =  Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window,'Level1', menu_return )
+                level_return =  level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()
+                quit()
+            else:
+                pass
 
 
 
-
-
-
-            # Verificação de eventos
-            """for evento in pygame.event.get():
-                if evento.type == pygame.QUIT:
-                    rodando = False  # Sai do loop se o evento for de fechamento
-                elif evento.type == pygame.KEYDOWN:
-                    if evento.key == pygame.K_w:
-                        # Lógica para tecla W pressionada
-                        print("Tecla W pressionada")
-                    elif evento.key == pygame.K_s:
-                        # Lógica para tecla S pressionada
-                        print("Tecla S pressionada")
-                    elif evento.key == pygame.K_a:
-                        # Lógica para tecla A pressionada
-                        print("Tecla A pressionada")
-                    elif evento.key == pygame.K_d:
-                        # Lógica para tecla D pressionada
-                        print("Tecla D pressionada")
-                elif evento.type == pygame.MOUSEBUTTONDOWN:
-                    if evento.button == 1:
-                        # Lógica para clique do botão esquerdo do mouse
-                        print("Botão esquerdo do mouse pressionado:", evento.pos)
-                    elif evento.button == 3:
-                        # Lógica para clique do botão direito do mouse
-                        print("Botão direito do mouse pressionado:", evento.pos)
-
-            # Lógica de atualização (aqui você desenharia e atualizaria o jogo)
-            # Adicione aqui o código para desenhar seus objetos
-            pygame.display.flip()  # Atualiza a tela
-
-        # Encerramento do Pygame
-        pygame.quit()
-        sys.exit()
-        """
 
--- code/Menu.py
+++ code/Menu.py
@@ -15,6 +15,7 @@ class Menu:
 
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
@@ -22,13 +23,32 @@ class Menu:
             self.menu_text(50, text='Mountain', text_color=(255, 128, 0), text_center_pos=((WIN_WIDTH / 2), 70))
             self.menu_text(50, text='Shooter', text_color=(255, 128, 0), text_center_pos=((WIN_WIDTH / 2), 120))
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, text=MENU_OPTION[i], text_color=(255, 255, 255), text_center_pos=((WIN_WIDTH / 2), 200+25*i))
-
+                if i == menu_option:
+                    self.menu_text(20, text=MENU_OPTION[i], text_color=(0, 0, 0),
+                                   text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, text=MENU_OPTION[i], text_color=(255, 255, 255), text_center_pos=((WIN_WIDTH / 2), 200+25*i))
             pygame.display.flip()
+
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     pygame.quit()
                     quit()
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) -1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if evento.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if evento.key == pygame.K_RETURN:
+                        return  MENU_OPTION[menu_option]
+
+
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
